d78eb90fe79f6263fe8d26afec627d47e7e4c107
김다영 <darong9494@gmail.com>
2026-03-13 10:05:52 +0900

    feat:스페이스바 이벤트 처리

--- src/test04/Bubble.java
+++ src/test04/Bubble.java
@@ -30,7 +30,7 @@ public class Bubble extends JLabel {
         this.y = y;
     }
 
-
+    // DI
     public Bubble(Player player) {
         this.player = player;
         initData();
@@ -44,9 +44,9 @@ public class Bubble extends JLabel {
     private void setInitLayout() {
         x = player.getX();
         y = player.getY();
+        setIcon(bubbleIcon);
         setSize(50,50);
         setLocation(x,y);
         setVisible(true);
     }
 } // end of class
-
